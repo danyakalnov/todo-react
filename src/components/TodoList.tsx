c42ded6f22d9cfd951ab137707141ba39b20e8dd
Данила Кальнов <danyakalnov@gmail.com>
2021-01-11 00:49:52 +0300

    Todo-list component

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core';
+import { getTasks } from '../api/todos';
+import { Task } from '../types/todos';
+import { TodoListItem } from './TodoListItem';
+import { CreateTodoList } from './CreateTodoItem';
+
+const useStyles = makeStyles({
+  todoList: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'start',
+    alignItems: 'center',
+    width: '100%',
+    height: 'auto',
+  },
+});
+
+export const TodoList: React.FC = () => {
+  const [tasks, setTasks] = useState<Array<Task>>([]);
+  const styles = useStyles();
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      const tasks = await getTasks();
+      setTasks(tasks ?? []);
+    };
+    fetchTasks();
+  }, []);
+
+  return (
+    <div className={styles.todoList}>
+      <CreateTodoList setTasks={setTasks} />
+      {tasks.map((task: Task) => (
+        <TodoListItem taskText={task.taskText} isDone={task.isDone} key={task.id} />
+      ))}
+    </div>
+  );
+};
